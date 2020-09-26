f74f972e6cb72d67a4d16ef958e2d8210e26f782
takidog <wow@taki.dog>
2020-09-26 23:38:49 +0800

    Add custom field config

--- src/utils/config.py
+++ src/utils/config.py
@@ -6,3 +6,18 @@ except KeyError:
     REDIS_URL = 'redis://127.0.0.1:6379'
 
 ANNOUNCEMENT_REQUIRED_FIELD = ["title"]
+
+ANNOUNCEMENT_FIELD = {
+    "title": {"type": str, "allow_user_set": True, "default": "Title not set"},
+    "id": {"type": int, "allow_user_set": False, "default": 0},
+    "publishedAt": {"type": str, "allow_user_set": False, "default": None},
+    "weight": {"type": int, "allow_user_set": True, "default": 0},
+    "url": {"type": str, "allow_user_set": True, "default": None},
+    "imgUrl": {"type": str, "allow_user_set": True, "default": None},
+    "description": {"type": str, "allow_user_set": True, "default": None},
+    "location": {"type": str, "allow_user_set": True, "default": None},
+    "expireTime": {"type": str, "allow_user_set": True, "default": None},
+    "tag": {"type": list, "allow_user_set": True, "default": []}
+}
+
+MAX_TAGS_LIMIT = 20
