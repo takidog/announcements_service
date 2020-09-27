0a1c52b018824daf096f97c69941625e2f5bba24
takidog <wow@taki.dog>
2020-09-27 17:28:01 +0800

    Add get/add/update/delete path

--- src/web-server.py
+++ src/web-server.py
@@ -1,19 +1,36 @@
 import falcon
-from auth import auth_middleware
-from view import api
-from view import user
-from view import library
-from view import leave
-from view import bus
-from view import news
-# pylint: disable=invalid-name
-app = falcon.API(middleware=[auth_middleware])
-
-# app.add_route('/oauth/token', )
-app.add_route('/news/announcements', news.Announcements())
-app.add_route('/news/announcements/{news_id}', news.AnnouncementsById())
-app.add_route('/news/announcements/all', news.AnnouncementsAll())
-# app.add_route('/news/school', news.acadNews())
-app.add_route('/news/announcements/add', news.NewsAdd())
-app.add_route('/news/announcements/update/{news_id}', news.NewsUpdate())
-app.add_route('/news/announcements/remove/{news_id}', news.NewsRemove())
+from view import announcement
+from announcements.announcement import AnnouncementService
+from cache.announcements_cache import CacheManager
+app = falcon.API()
+acs = AnnouncementService()
+cache_manager = CacheManager()
+app.add_route(
+    '/announcements',
+    announcement.Announcements(cache_manager=cache_manager)
+)
+app.add_route(
+    '/announcements/{announcement_id}',
+    announcement.AnnouncementsById(announcement_service=acs)
+)
+app.add_route(
+    '/announcements/add',
+    announcement.AnnouncementsAdd(
+        cache_manager=cache_manager,
+        announcement_service=acs
+    )
+)
+app.add_route(
+    '/announcements/update/{announcement_id}',
+    announcement.AnnouncementsUpdate(
+        cache_manager=cache_manager,
+        announcement_service=acs
+    )
+)
+app.add_route(
+    '/announcements/delete/{announcement_id}',
+    announcement.AnnouncementsRemove(
+        cache_manager=cache_manager,
+        announcement_service=acs
+    )
+)
