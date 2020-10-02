799d75b10a0eae70f8ab78e36a73c7c809d7760a
takidog <wow@taki.dog>
2020-10-02 22:39:32 +0800

    Rename announcement to announcement_view

--- src/web-server.py
+++ src/web-server.py
@@ -1,39 +1,44 @@
 import falcon
-from view import announcement
+from view import announcement_view
+from view import auth
 from announcements.announcement import AnnouncementService
 from cache.announcements_cache import CacheManager
+from auth.auth import AuthService
 app = falcon.API()
+auth_service = AuthService()
 acs = AnnouncementService()
 cache_manager = CacheManager()
+app = falcon.API(middleware=[auth_service.auth_middleware])
+
 app.add_route(
     '/announcements',
-    announcement.Announcements(cache_manager=cache_manager)
+    announcement_view.Announcements(cache_manager=cache_manager)
 )
 app.add_route(
     '/announcements/{announcement_id}',
-    announcement.AnnouncementsById(announcement_service=acs)
+    announcement_view.AnnouncementsById(announcement_service=acs)
 )
 app.add_route(
     '/announcements/tags',
-    announcement.AnnouncementsTagCount(cache_manager=cache_manager)
+    announcement_view.AnnouncementsTagCount(cache_manager=cache_manager)
 )
 app.add_route(
     '/announcements/add',
-    announcement.AnnouncementsAdd(
+    announcement_view.AnnouncementsAdd(
         cache_manager=cache_manager,
         announcement_service=acs
     )
 )
 app.add_route(
     '/announcements/update/{announcement_id}',
-    announcement.AnnouncementsUpdate(
+    announcement_view.AnnouncementsUpdate(
         cache_manager=cache_manager,
         announcement_service=acs
     )
 )
 app.add_route(
     '/announcements/delete/{announcement_id}',
-    announcement.AnnouncementsRemove(
+    announcement_view.AnnouncementsRemove(
         cache_manager=cache_manager,
         announcement_service=acs
     )
